aad0d18ae1e5f0011d5636725ed3b34e44a99a71
Your Name <you@example.com>
2021-03-29 03:03:00 -0700

    ad

--- src/App.js
+++ src/App.js
@@ -70,39 +70,42 @@
 
 
 import { useState } from "react";
-import Countdown from "./Countdown/Countdown";
+import Countdown from "./components/Countdown/Countdown";
 const App = () => {
   const [a, setA] = useState(randomNumber(50));
   const [b, setB] = useState(randomNumber(50));
-  
-  const [m, setM] = useState(randomNumber(50));
   const [answer, setAnswer] = useState("?");
-  const [countdown, setCountdown] = useState(20);
+  const [countdown, setCountdown] = useState(5);
   function randomNumber(limit) {
     return Math.round(Math.random() * limit);
   }
   function checkAnswer() {
     const c = a + b;
-
     if (c == answer) {
       setA(randomNumber(50));
       setB(randomNumber(50));
-      setM(randomNumber(0))
       setAnswer("?");
-      setCountdown(20);
+      setCountdown(5);
     }
     else {
       alert("No");
     }
   }
+  let userInput = null;
+  if (countdown > 0) {
+    userInput = (
+      <>
+        <input type="number" value={answer} onChange={({ target }) => setAnswer(target.value)} />
+        <button onClick={() => checkAnswer()}>Ok</button>
+      </>
+    ) 
+  }
   return (
     <div className="App">
       {a} + {b} = {answer}
       <Countdown countdown={countdown} setCountdown={setCountdown} />
-      <input type="number" value={answer} onChange={({ target }) => setAnswer(target.value)} />
-      <button onClick={() => checkAnswer()}>Ok</button>
+      {userInput}
     </div>
   );
 }
 export default App;
-
