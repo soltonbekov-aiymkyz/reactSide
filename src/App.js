e151f803b02590e3119a182c98bc313ebf12dae7
Your Name <you@example.com>
2021-03-29 01:21:38 -0700

    added new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Countdown from "./components/Countdown/Countdown"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const [Countdown,setCountdown]=useState(20);
 }
 
-export default App;
+
+
