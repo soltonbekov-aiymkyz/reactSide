8df67e5f32502830544d7695281215b5d3b15246
Your Name <you@example.com>
2021-03-29 02:31:36 -0700

    added new cha

--- src/App.js
+++ src/App.js
@@ -1,69 +1,67 @@
-// import { useState } from "react"
-// import Countdown from "./components/Countdown/Countdown"
-// const App = () => {
-//   const [Countdown,setCountdown]=useState(20);
-//   const [a,setA]=useState(Math.round(Math.random)); 
-//   const [b,setB]=useState(Math.round(Math.random));
-//   const [c,setC]=useState("?");
-// }
-// function reset() {
-//   setA(Math.round(Math.random() * 50));
-//   setB(Math.round(Math.random() * 50));
-//   setC("?");
-//   setCountdown(20);
-// }
-// function checkAnswer(){
-//   if (a + b == c){
-//     reset();
-//   }
-//   else {
-//     alert("No");
-//   }
-// }
-// return (
-//   <div className="App">
-//     {a} + {b} + {c}
-//     <Countdown countdown={countdown}
-//      setCountdown={setCountdown} />
-//      <input type="text"
-//       onChange={({ target })=> setC(target.value)
-//       <button onClick ={checkAnswer}>Ok</button>
-//   </div>
-// )
-// }
-// export default App
 
-
-import {useState} from "react"
-import Plus from "./components/Countdown/Countdown"
-function App() {
-  const[plus , setPlus] = useState(20);
-  const[a , setA] = useState(Math.round(Math.random()));
-  const[b , setB] = useState(Math.round(Math.random()));
-  const[c , setC] = useState("?");
-  function reset() {
-    setA(Math.round(Math.random() * 50));
-    setB(Math.round(Math.random() * 50));
-    setC("?");
-    setPlus(20);
+// import {useState} from "react"
+// import Plus from "./components/Countdown/Countdown"
+// function App() {
+//   const[plus , setPlus] = useState(20);
+//   const[a , setA] = useState(Math.round(Math.random()));
+//   const[b , setB] = useState(Math.round(Math.random()));
+//   const[c , setC] = useState("?");
+//   function reset() {
+//     setA(Math.round(Math.random() * 50));
+//     setB(Math.round(Math.random() * 50));
+//     setC("?");
+//     setPlus(20);
+//   }
+//   function CheckAnswer() {
+//     if(a + b == c){
+//       reset();
+//     }
+//     else{
+//       alert("No")
+//     }
+//   }
+//   return (
+//     <div>
+//       {a} + {b} = {c}
+//       <Plus plus={plus}
+//       setPlus = {setPlus} />
+//       <input type="text"
+//       onChange = {({target}) => setC(target.value)} />
+//       <button onClick = {CheckAnswer}>Ok</button>
+//     </div>
+//   )
+// }
+// export default App;import { useState } from "react";
+import Countdown from "./components/Countdown/Countdown";
+const App = () => {
+  const [a, setA] = useState(randomNumber(50));
+  const [b, setB] = useState(randomNumber(50));
+  const [answer, setAnswer] = useState("?");
+  const [countdown, setCountdown] = useState(20);
+  function randomNumber(limit) {
+    return Math.round(Math.random() * limit);
   }
-  function CheckAnswer() {
-    if(a + b == c){
-      reset();
+  function checkAnswer() {
+    const c = a + b;
+    if (c == answer) {
+      setA(randomNumber(50));
+      setB(randomNumber(50));
+      setAnswer("?");
+      setCountdown(20);
     }
-    else{
-      alert("No")
+    else {
+      alert("No");
     }
   }
   return (
-    <div>
-      {a} + {b} = {c}
-      <Plus plus={plus}
-      setPlus = {setPlus} />
-      <input type="text"
-      onChange = {({target}) => setC(target.value)} />
-      <button onClick = {CheckAnswer}>Ok</button>
+    <div className="App">
+      {a} + {b} = {answer}
+      <Countdown countdown={countdown} setCountdown={setCountdown} />
+      <input type="number" value={answer} onChange={({ target }) => setAnswer(target.value)} />
+      <button onClick={() => checkAnswer()}>Ok</button>
     </div>
-  )
+  );
 }
 export default App;
+
+
--- src/Countdown/Countdown.js
+++ src/Countdown/Countdown.js
@@ -1,61 +1,38 @@
-// import { useEffect } from "react"
-// import classes from "./Countdown.module.css"
 
-// const Countdown = ({ countdown, setCountdown }) => {
-//     useEffect(function () {
-//         if (!countdown) return;
-//     }
-
-// const internal = setInterval(() => {
-//         setCountdown(countdown - 1)
-//     }, 1000);
-
-//     return () => clearInterval(internal);
-// }, [countdown]);
-
-// return (
-//     <div className={classes.Countdown}>{Countdown}</div>
-// )
-// }
-// export default Countdown
-
-// import { useEffect } from "react"
-// import classes from "./Countdown.module.css"
-// const Countdown = ({ countdown, setCountdown }) => {
-//     useEffect(function () {
-//         if (!countdown) return;
-//     }
-//      const internal = setInterval(() => {
-//         setCountdown(countdown - 1)
-//     }, 1000);
-
-//     return () => clearInterval(internal);
-// }, [countdown]);
-
-// return (
-//     <div className={classes.Countdown}>{countdown}</div>
-// )
-// }
-// export default Countdown
+// import {useEffect} from "react";
+// import classes from "./Countdown.module.css";
+// const Countdown  = ({countdown, setCountdown})=>{
+//     useEffect(function(){
+//         if(!countdown)return;
+//         const interval = setInterval(() => {
+//             setCountdown(plus - 1)
+//         },1000)
+//     return () => clearInterval(interval);
+// }, [plus])
+// return(
+//     <div className={classes.Countdown}>
+//       {countdown}
+//     </div>
+// )}
+// export default Countdown;
 
 
-import {useEffect} from "react";
+
+import { useEffect } from "react";
 import classes from "./Countdown.module.css";
-const Countdown  = ({countdown, setCountdown})=>{
-    useEffect(function(){
-        if(!countdown)return;
-        const interval = setInterval(() => {
-            setCountdown(plus - 1)
-        },1000)
+const Countdown = ({ countdown, setCountdown }) => {
+  useEffect(() => {
+    if (!countdown) return;
+    const interval = setInterval(function() {
+      setCountdown(countdown - 1);
+    }, 1000);
     return () => clearInterval(interval);
-}, [plus])
-return(
-    <div className={classes.Countdown}>
-      {countdown}
-    </div>
-)}
+  }, [countdown]);
+  return (
+    <div className={classes.Countdown}>{countdown}</div>
+  );
+}
 export default Countdown;
-
 
 
 
